390bcdd5cd3683456ff98eaa9aa5e6da6748d03c
18016375 <18016375@myrp.edu.sg>
2020-05-14 11:53:48 +0800

    activites 1-10000

--- app/src/main/java/com/example/demodatabase/DBHelper.java
+++ app/src/main/java/com/example/demodatabase/DBHelper.java
@@ -1,12 +1,23 @@
 package com.example.demodatabase;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import androidx.annotation.Nullable;
+
 public class DBHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VER = 1;
+    private static final String DATABASE_NAME = "task.db";
+
+    public DBHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VER);
+    }
+
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        
+
     }
 
     @Override
